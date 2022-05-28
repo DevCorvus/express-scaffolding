b1a7a2b845c35854781686c255ca28309626c5e0
Luis Portillo <devcorvus.contact@gmail.com>
2022-05-27 21:32:02 -0400

    LICENSE typo fixed and dotenv conditional simplifed

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -3,7 +3,7 @@ import { getEnv } from '../config/env';
 export const loadLocalEnvIfRequired = async () => {
   const { NODE_ENV } = getEnv();
 
-  if (!NODE_ENV || NODE_ENV === 'development' || NODE_ENV === 'test') {
+  if (NODE_ENV !== 'production') {
     const dotenv = (await import('dotenv')).default;
     dotenv.config();
   }
